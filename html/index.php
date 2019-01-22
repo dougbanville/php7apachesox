e990df6c3dfae9841f69410df63c8501fa8318ff
Doug Banville <doug.banville@rte.ie>
2019-01-22 09:58:45 +0000

    updated dotEnv path

--- html/index.php
+++ html/index.php
@@ -1,7 +1,7 @@
 <?php
 require 'vendor/autoload.php';
-$dotenv = new Dotenv\Dotenv('../../');
+$dotenv = new Dotenv\Dotenv(__DIR__.'/../');
 $dotenv->load();
-$version = getenv("version");
-echo "<h1>Version $version</h2>";
+//$version = getenv("version");
+//echo "<h1>Version $version</h2>";
 echo phpinfo();
