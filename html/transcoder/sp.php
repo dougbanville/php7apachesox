cae723a4d8ccb317b2a9ec16f2c3d53ebaf9612c
Doug Banville <doug.banville@rte.ie>
2019-07-08 17:26:50 +0100

    ffmpeg to stich video init

--- html/transcoder/sp.php
+++ html/transcoder/sp.php
@@ -6,7 +6,7 @@ use Kreait\Firebase\ServiceAccount;
 $dotenv->load();
 echo __DIR__;
 echo getenv("version");
-$serviceAccount = ServiceAccount::fromJsonFile('../../firebase.json');
+$serviceAccount = ServiceAccount::fromJsonFile('../firebase.json');
 $firebase = (new Factory)
     ->withServiceAccount($serviceAccount)
     ->create();
@@ -18,32 +18,31 @@ $audioClip = $db->getReference('audioclips')
 print_r($audioClip);
 echo "hello";
 
-
 echo getenv("version");
-    /*
-    $obj = array("title"=>"RTÉ News");
+/*
+$obj = array("title"=>"RTÉ News");
 
-    $obj["title"]  = "RTÉ News";
-    $obj["url"] ="https://www.rte.ie";
-    $obj["description"] ="descript";
-    $obj["language"] ="en-ie";
-    $obj["category"] ="News &amp; Politics";
-    $obj["copyrigh"] = date("Y")." RTÉ";
-    $obj["ttl"] = 60;
-    $obj["imageUrl"] ="https://s3-eu-west-1.amazonaws.com/radiodj-alexa-live/english-news/RTE_News_Square_1600.png";
-    $obj["imageLink"]  ="https://dffsd";
-    $obj["audioUrl"] ="https://radiodj-alexa-live.s3.amazonaws.com/english-news/audio.mp3";
-    $obj["guid"] ="1234567";
-    $obj["pubDate"] =gmdate("D, d M Y H:i:s +0000");
-    $obj["author"] ="doug";
-    $obj["link"] ="link";
-    $obj["audioUrl"] ="hjkhkjh";
-    $obj["audioLength"] =8787887987;
-    $obj["audioType"] ="audio/mpeg";
-    $obj["duration"] =120;
+$obj["title"]  = "RTÉ News";
+$obj["url"] ="https://www.rte.ie";
+$obj["description"] ="descript";
+$obj["language"] ="en-ie";
+$obj["category"] ="News &amp; Politics";
+$obj["copyrigh"] = date("Y")." RTÉ";
+$obj["ttl"] = 60;
+$obj["imageUrl"] ="https://s3-eu-west-1.amazonaws.com/radiodj-alexa-live/english-news/RTE_News_Square_1600.png";
+$obj["imageLink"]  ="https://dffsd";
+$obj["audioUrl"] ="https://radiodj-alexa-live.s3.amazonaws.com/english-news/audio.mp3";
+$obj["guid"] ="1234567";
+$obj["pubDate"] =gmdate("D, d M Y H:i:s +0000");
+$obj["author"] ="doug";
+$obj["link"] ="link";
+$obj["audioUrl"] ="hjkhkjh";
+$obj["audioLength"] =8787887987;
+$obj["audioType"] ="audio/mpeg";
+$obj["duration"] =120;
 
-    $fileName="audio/audio.xml";
-    */
+$fileName="audio/audio.xml";
+ */
 
 //Header('Content-type: text/xml');
 
@@ -57,4 +56,4 @@ $date2=date_create("2013-12-12");
 $diff=date_diff($date1,$date2);
 
 echo $diff->d;
-*/
+ */
